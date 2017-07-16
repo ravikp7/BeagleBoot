2647ab6e78925d1a27934925a94051d96a3dc9ed
ravikp7 <7ravikp@gmail.com>
2017-07-16 15:56:15 +0530

    add css style loaders in webpack

--- src/app/index.js
+++ src/app/index.js
@@ -1,12 +1,12 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-
+require('../css/box.css');
 
 // Create component
 class TodoComponent extends React.Component{
     render(){
         return(
-            <div>
+            <div className='box'>
             <h1>UI goes here! </h1>
             </div>
         );
