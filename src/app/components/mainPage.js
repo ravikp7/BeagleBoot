f7339578031f49a07fb9cd5424ed2dac966fdb45
ravikp7 <7ravikp@gmail.com>
2017-07-19 11:43:15 +0530

    add ums button functionality and update progress bar with status

--- src/app/components/mainPage.js
+++ src/app/components/mainPage.js
@@ -2,10 +2,31 @@ var React = require('react');
 import Block from './Block';
 import Progress from './progress';
 import Info from './info';
+var BB = window.require('beagle-boot');
 
 class MainPage extends React.Component{
     constructor(props){
         super(props);
+        this.state = {progressValue: 0, infoText: 'Go ahead! click USB Mass Storage button to begin'};
+        this.umsClick = this.umsClick.bind(this);
+    }
+
+    umsClick(){
+        var ums = BB.usbMassStorage();
+        ums.on('progress', function(status){
+            console.log(status);
+            this.setState(()=>({progressValue: status.complete, infoText: status.description}));
+        }.bind(this));
+
+        ums.on('done', ()=>{
+            console.log('Select Image');
+            this.setState(()=>({progressValue: 0, infoText: 'Select image for flashing'}));
+        });
+
+        ums.on('error', function(error){
+            console.log('Error: '+error);
+            this.setState(()=>({progressValue: 0, infoText: 'Please disconnect usb cable, connect and try again'}));
+        });
     }
 
     render(){
@@ -13,16 +34,16 @@ class MainPage extends React.Component{
             <div>
                 <div id='bl'>    
                     <section id='blocks'>
-                        <Block id='usb' task='USB Mass Storage' imgURL='./images/usb-memory.png'/>
+                        <Block id='usb' task='USB Mass Storage' imgURL='./images/usb-memory.png' handleClick={this.umsClick}/>
                         <Block id='img' task='Select Image' imgURL='./images/image.png'/>
                         <Block id='flash' task='Flash' imgURL='./images/flash.png'/>
                     </section>
                 </div>
                 <div id='prog'>
-                    <Progress value='30'/>
+                    <Progress value={this.state.progressValue}/>
                 </div>
                 <div id='info'>
-                    <Info value="Info here!"/>
+                    <Info value={this.state.infoText}/>
                 </div>
             </div>
         );
