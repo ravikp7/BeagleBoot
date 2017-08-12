6b24c33708e0f885927de9a781784bdcc281224b
ravikp7 <7ravikp@gmail.com>
2017-08-12 20:51:42 +0530

    usb mass storage with sudo-prompt

--- src/app/components/mainPage.js
+++ src/app/components/mainPage.js
@@ -1,10 +1,16 @@
-var React = require('react');
+const React = require('react');
+const BB = window.require('beagle-boot');
+const app = window.require('electron').remote; 
+const dialog = app.dialog;
+const fork = window.require('child_process').fork;
+const sudo = window.require('sudo-prompt');
+const ipc = window.require('node-ipc');
+const fs = window.require('fs');
+
+// React Components
 import Block from './Block';
 import Progress from './progress';
 import Info from './info';
-var BB = window.require('beagle-boot');
-var app = window.require('electron').remote; 
-var dialog = app.dialog;
 
 class MainPage extends React.Component{
     constructor(props){
@@ -15,21 +21,42 @@ class MainPage extends React.Component{
     }
 
     umsClick(){
-        var ums = BB.usbMassStorage();
-        ums.on('progress', function(status){
-            console.log(status);
-            this.setState(()=>({progressValue: status.complete, infoText: status.description}));
+       
+        // IPC
+        window.process.env.IPC_SERVER_ID = `BB-server-${window.process.pid}`;
+        window.process.env.IPC_CLIENT_ID = `BB-client-${window.process.pid}`;
+
+        ipc.config.id = window.process.env.IPC_SERVER_ID;
+        ipc.config.silent = true;
+        ipc.serve();
+        ipc.server.on('start', () => {
+            var child = fork('./lib/elevate.js', [],{
+                silent: true, // in order for the stdin, stdout and stderr to get piped back to the parent process
+                env: window.process.env
+            });
+            child.stdout.on('data', (data)=>{
+                console.log(`${data}`);
+            });
+
+            child.stderr.on('data', (data)=>{
+                console.log(`${data}`);
+                child.kill();
+            });
+
+            child.on('close', (code) => {
+                console.log(`child process exited with code ${code}`);
+            });
+        });
+        ipc.server.start();
+
+        ipc.server.on('message', function(data){
+            console.log(data);
+            try{
+                var status = JSON.parse(data);
+            }
+            catch(e){ }
+            if(status && status.description) this.setState(()=>({progressValue: status.complete, infoText: status.description}));
         }.bind(this));
-
-        ums.on('done', ()=>{
-            console.log('Select Image');
-            this.setState(()=>({progressValue: 0, infoText: 'Select image for flashing'}));
-        });
-
-        ums.on('error', function(error){
-            console.log('Error: '+error);
-            this.setState(()=>({progressValue: 0, infoText: 'Please disconnect usb cable, connect and try again'}));
-        });
     }
 
     selectImage(){
