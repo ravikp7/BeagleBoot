e1a407204bc939b22b131b26956dc9b30b9510c4
ravikp7 <7ravikp@gmail.com>
2017-08-14 03:04:31 +0530

    use promise to start IPC server and return socket on resolution

--- src/app/components/mainPage.js
+++ src/app/components/mainPage.js
@@ -12,42 +12,23 @@ import Block from './Block';
 import Progress from './progress';
 import Info from './info';
 
+var IPCserverStarted;   // Promise, returns socket on resolution
+
+
 class MainPage extends React.Component{
     constructor(props){
         super(props);
         this.state = {progressValue: 0, infoText: 'Go ahead! click USB Mass Storage button to begin'};
         this.umsClick = this.umsClick.bind(this);
         this.selectImage = this.selectImage.bind(this);
-    }
 
-    umsClick(){
-       
-        // IPC
+        // IPC Server
         window.process.env.IPC_SERVER_ID = `BB-server-${window.process.pid}`;
         window.process.env.IPC_CLIENT_ID = `BB-client-${window.process.pid}`;
 
         ipc.config.id = window.process.env.IPC_SERVER_ID;
         ipc.config.silent = true;
         ipc.serve();
-        ipc.server.on('start', () => {
-            var child = fork('./lib/elevate.js', [],{
-                silent: true, // in order for the stdin, stdout and stderr to get piped back to the parent process
-                env: window.process.env
-            });
-            child.stdout.on('data', (data)=>{
-                console.log(`${data}`);
-            });
-
-            child.stderr.on('data', (data)=>{
-                console.log(`${data}`);
-                child.kill();
-            });
-
-            child.on('close', (code) => {
-                console.log(`child process exited with code ${code}`);
-            });
-        });
-        ipc.server.start();
 
         ipc.server.on('message', function(data){
             console.log(data);
@@ -57,6 +38,52 @@ class MainPage extends React.Component{
             catch(e){ }
             if(status && status.description) this.setState(()=>({progressValue: status.complete, infoText: status.description}));
         }.bind(this));
+    }
+
+        
+
+    umsClick(){
+        // Promise, starts IPC server and returns socket for client on resolution
+        IPCserverStarted = new Promise((resolve, reject)=>{
+
+            ipc.server.on('start', () => {
+                
+                const child = fork('./lib/elevate.js', [],{
+                    silent: true, // in order for the stdin, stdout and stderr to get piped back to the parent process
+                    env: window.process.env
+                });
+                child.stdout.on('data', (data)=>{
+                    console.log(`${data}`);
+                });
+
+                child.stderr.on('data', (data)=>{
+                    console.log(`${data}`);
+                    child.kill();
+                    reject();
+                });
+
+                child.on('close', (code) => {
+                    console.log(`child process exited with code ${code}`);
+                });
+
+                ipc.server.on('script', function(data, socket){
+                    resolve(socket);
+                });
+            });
+
+            ipc.server.start();
+        });
+        
+        IPCserverStarted.then((socket)=>{
+            ipc.server.emit(
+                socket,
+                'script',
+                {
+                    id: ipc.config.id,
+                    script: 'start'
+                }
+            );
+        }); 
     }
 
     selectImage(){
