a3c2c7b0402a1fd5799c5f5a53015b5f32841126
ravikp7 <7ravikp@gmail.com>
2017-08-16 01:31:45 +0530

    move state management and ipc server to parent component

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -1,39 +1,287 @@
 var React = require('react');
-require('../../css/main.css');
-require('../../css/nav.css');
+const app = window.require('electron').remote; 
+const dialog = app.dialog;
+const fork = window.require('child_process').fork;
+const sudo = window.require('sudo-prompt');
+const ipc = window.require('node-ipc');
+const fs = window.require('fs');
+const drivelist = window.require('drivelist');
+const path = window.require('path');
+
+// React Components
 import Block from './Block';
 import MainPage from './mainPage';
 import Settings from './settings';
 import About from './about';
 
+// StyleSheets
+require('../../css/main.css');
+require('../../css/nav.css');
+
+
+var IPCserverStarted;   // Promise, returns socket on resolution
+var imagePath;
+
 // Main App
 class App extends React.Component{
     constructor(props){
         super(props);
-        this.state = {window: 'main'};
+        this.state = {
+            progress: {
+                value: 0,
+                infoText: 'Go ahead! click USB Mass Storage button to begin'
+            },
+            buttonState: {
+                ums: true,
+                img: false,
+                flash: false
+            },
+            window: 'main'
+        };
         this.settingsClick = this.settingsClick.bind(this);
         this.mainClick = this.mainClick.bind(this);
         this.aboutClick = this.aboutClick.bind(this);
+        this.umsClick = this.umsClick.bind(this);
+        this.selectImage = this.selectImage.bind(this);
+        this.writeImage = this.writeImage.bind(this);
+
+        // IPC Server
+        window.process.env.IPC_SERVER_ID = `BB-server-${window.process.pid}`;
+        window.process.env.IPC_CLIENT_ID = `BB-client-${window.process.pid}`;
+
+        ipc.config.id = window.process.env.IPC_SERVER_ID;
+        ipc.config.silent = true;
+        ipc.serve();
+
+        ipc.server.on('message', function(data){
+            console.log(data);
+            try{
+                var status = JSON.parse(data);
+            }
+            catch(e){ }
+            if(status && status.description) this.setState((prevState)=>{
+                return {
+                    progress: {
+                        value: status.complete,
+                        infoText: status.description
+                    },
+                    buttonState:{
+                        ums: prevState.buttonState.ums,
+                        img: prevState.buttonState.img,
+                        flash: prevState.buttonState.flash
+                    },
+                    window: prevState.window
+                }
+            });
+
+            if(status && status.done) this.setState((prevState)=>{
+                return {
+                    progress: {
+                        value: 0,
+                        infoText: 'Select Image for flashing'
+                    },
+                    buttonState:{
+                        ums: false,
+                        img: true,
+                        flash: false
+                    },
+                    window: prevState.window
+                }
+            });
+
+            if(status && status.type)this.setState((prevState)=>{
+                return {
+                    progress: {
+                        value: status.percentage,
+                        infoText: 'Writing Image => Percentage: '+status.percentage.toFixed(2)+'%  ---- Speed: '+(status.speed/(1024*1024)).toFixed(2)+'MB/s ---- ETA: '+Math.floor(status.eta/60)+' min.'
+                    },
+                    buttonState:{
+                        ums: false,
+                        img: false,
+                        flash: false
+                    },
+                    window: prevState.window
+                }
+            });
+        }.bind(this));
     }
 
     settingsClick(){
-        this.setState(()=>({window: 'settings'}));
+        this.setState((prevState)=>{
+                return {
+                    progress: {
+                        value: prevState.progress.value,
+                        infoText: prevState.progress.infoText
+                    },
+                    buttonState:{
+                        ums: prevState.buttonState.ums,
+                        img: prevState.buttonState.img,
+                        flash: prevState.buttonState.flash
+                    },
+                    window: 'settings'
+                }
+            });
     }
 
     mainClick(){
-        this.setState(()=>({window: 'main'}));
+        this.setState((prevState)=>{
+                return {
+                    progress: {
+                        value: prevState.progress.value,
+                        infoText: prevState.progress.infoText
+                    },
+                    buttonState:{
+                        ums: prevState.buttonState.ums,
+                        img: prevState.buttonState.img,
+                        flash: prevState.buttonState.flash
+                    },
+                    window: 'main'
+                }
+            });
     }
 
     aboutClick(){
-        this.setState(()=>({window: 'about'}));
+        this.setState((prevState)=>{
+                return {
+                    progress: {
+                        value: prevState.progress.value,
+                        infoText: prevState.progress.infoText
+                    },
+                    buttonState:{
+                        ums: prevState.buttonState.ums,
+                        img: prevState.buttonState.img,
+                        flash: prevState.buttonState.flash
+                    },
+                    window: 'about'
+                }
+            });
     }
+
+    umsClick(){
+        // Promise, starts IPC server and returns socket for client on resolution
+        IPCserverStarted = new Promise((resolve, reject)=>{
+
+            ipc.server.on('start', () => {
+                
+                const child = fork('./lib/elevate.js', [],{
+                    silent: true, // in order for the stdin, stdout and stderr to get piped back to the parent process
+                    env: window.process.env
+                });
+                child.stdout.on('data', (data)=>{
+                    console.log(`${data}`);
+                });
+
+                child.stderr.on('data', (data)=>{
+                    console.log(`${data}`);
+                    child.kill();
+                    reject();
+                });
+
+                child.on('close', (code) => {
+                    console.log(`child process exited with code ${code}`);
+                });
+
+                ipc.server.on('script', function(data, socket){
+                    resolve(socket);
+                });
+            });
+
+            ipc.server.start();
+        });
+        
+        IPCserverStarted.then((socket)=>{
+            ipc.server.emit(
+                socket,
+                'script',
+                {
+                    id: ipc.config.id,
+                    script: 'usbMassStorage',
+                    device: '',
+                    size: '',
+                    img: ''
+                }
+            );
+        });
+    }
+
+    selectImage(){
+        dialog.showOpenDialog({title: 'Select OS image for Flashing' , filters:[{name: 'OS Images', extensions: ['img', 'xz']}]},(filePath) => {
+            if(filePath === undefined){
+                this.setState((prevState)=>{
+                    return {
+                        progress: {
+                            value: 0,
+                            infoText: 'No Image file is selected'
+                        },
+                        buttonState:{
+                            ums: false,
+                            img: true,
+                            flash: false
+                        },
+                        window: prevState.window
+                    }
+                });
+                return;
+            }
+            else{
+                imagePath = filePath[0];
+                this.setState((prevState)=>{
+                    return {
+                        progress: {
+                            value: 0,
+                            infoText: 'Selected Image: ' + path.basename(imagePath)
+                        },
+                        buttonState:{
+                            ums: false,
+                            img: true,
+                            flash: true
+                        },
+                        window: prevState.window
+                    }
+                });
+            }
+        });
+    }
+
+    writeImage(){
+        drivelist.list((error, drives)=>{
+            drives.forEach((drive)=>{
+                if(drive.description === 'UMS disk 0'){
+
+                    IPCserverStarted.then((socket)=>{
+                        ipc.server.emit(
+                            socket,
+                            'script',
+                            {
+                                id: ipc.config.id,
+                                script: 'imageWrite',
+                                device: drive.device,
+                                size: drive.size,
+                                img: imagePath
+                            }
+                        );
+                    });
+                }
+            });
+        });
+        
+    }
+
+
 
     render(){
 
         let WindowComponent = null;
 
         switch(this.state.window){
-            case 'main': WindowComponent = <MainPage/>;
+            case 'main': WindowComponent = <MainPage progressValue={this.state.progress.value}
+                                                    infoText={this.state.progress.infoText}
+                                                    umsState={this.state.buttonState.ums}
+                                                    imgState={this.state.buttonState.img}
+                                                    flashState={this.state.buttonState.flash}
+                                                    umsHandle={this.umsClick}
+                                                    imgHandle={this.selectImage}
+                                                    flashHandle={this.writeImage}/>;
             break;
 
             case 'settings': WindowComponent = <Settings handleClick={this.mainClick}/>;
@@ -59,7 +307,7 @@ class App extends React.Component{
                     {WindowComponent}
                 </div>
                 <div id='footer'>
-                    Footer Content here
+                    
                 </div>
             </div>
         );
--- src/app/components/mainPage.js
+++ src/app/components/mainPage.js
@@ -1,206 +1,14 @@
 const React = require('react');
-const app = window.require('electron').remote; 
-const dialog = app.dialog;
-const fork = window.require('child_process').fork;
-const sudo = window.require('sudo-prompt');
-const ipc = window.require('node-ipc');
-const fs = window.require('fs');
-const drivelist = window.require('drivelist');
-const path = window.require('path');
 
 // React Components
 import Block from './Block';
 import Progress from './progress';
 import Info from './info';
 
-var IPCserverStarted;   // Promise, returns socket on resolution
-var imagePath;
-
 
 class MainPage extends React.Component{
     constructor(props){
         super(props);
-        this.state = {
-            progress: {
-                value: 0,
-                infoText: 'Go ahead! click USB Mass Storage button to begin'
-            },
-            buttonState: {
-                ums: true,
-                img: false,
-                flash: false
-            }
-        };
-        this.umsClick = this.umsClick.bind(this);
-        this.selectImage = this.selectImage.bind(this);
-        this.writeImage = this.writeImage.bind(this);
-
-        // IPC Server
-        window.process.env.IPC_SERVER_ID = `BB-server-${window.process.pid}`;
-        window.process.env.IPC_CLIENT_ID = `BB-client-${window.process.pid}`;
-
-        ipc.config.id = window.process.env.IPC_SERVER_ID;
-        ipc.config.silent = true;
-        ipc.serve();
-
-        ipc.server.on('message', function(data){
-            console.log(data);
-            try{
-                var status = JSON.parse(data);
-            }
-            catch(e){ }
-            if(status && status.description) this.setState((prevState)=>{
-                return {
-                    progress: {
-                        value: status.complete,
-                        infoText: status.description
-                    },
-                    buttonState:{
-                        ums: prevState.buttonState.ums,
-                        img: prevState.buttonState.img,
-                        flash: prevState.buttonState.flash
-                    }
-                }
-            });
-
-            if(status && status.done) this.setState((prevState)=>{
-                return {
-                    progress: {
-                        value: 0,
-                        infoText: 'Select Image for flashing'
-                    },
-                    buttonState:{
-                        ums: false,
-                        img: true,
-                        flash: false
-                    }
-                }
-            });
-
-            if(status && status.type)this.setState((prevState)=>{
-                return {
-                    progress: {
-                        value: status.percentage,
-                        infoText: 'Writing Image => Percentage: '+status.percentage.toFixed(2)+'%  ---- Speed: '+(status.speed/(1024*1024)).toFixed(2)+'MB/s ---- ETA: '+Math.floor(status.eta/60)+' min.'
-                    },
-                    buttonState:{
-                        ums: false,
-                        img: false,
-                        flash: false
-                    }
-                }
-            });
-        }.bind(this));
-    }
-
-        
-
-    umsClick(){
-        // Promise, starts IPC server and returns socket for client on resolution
-        IPCserverStarted = new Promise((resolve, reject)=>{
-
-            ipc.server.on('start', () => {
-                
-                const child = fork('./lib/elevate.js', [],{
-                    silent: true, // in order for the stdin, stdout and stderr to get piped back to the parent process
-                    env: window.process.env
-                });
-                child.stdout.on('data', (data)=>{
-                    console.log(`${data}`);
-                });
-
-                child.stderr.on('data', (data)=>{
-                    console.log(`${data}`);
-                    child.kill();
-                    reject();
-                });
-
-                child.on('close', (code) => {
-                    console.log(`child process exited with code ${code}`);
-                });
-
-                ipc.server.on('script', function(data, socket){
-                    resolve(socket);
-                });
-            });
-
-            ipc.server.start();
-        });
-        
-        IPCserverStarted.then((socket)=>{
-            ipc.server.emit(
-                socket,
-                'script',
-                {
-                    id: ipc.config.id,
-                    script: 'usbMassStorage',
-                    device: '',
-                    size: '',
-                    img: ''
-                }
-            );
-        });
-    }
-
-    selectImage(){
-        dialog.showOpenDialog({title: 'Select OS image for Flashing' , filters:[{name: 'OS Images', extensions: ['img', 'xz']}]},(filePath) => {
-            if(filePath === undefined){
-                this.setState((prevState)=>{
-                    return {
-                        progress: {
-                            value: 0,
-                            infoText: 'No Image file is selected'
-                        },
-                        buttonState:{
-                            ums: false,
-                            img: true,
-                            flash: false
-                        }
-                    }
-                });
-                return;
-            }
-            else{
-                imagePath = filePath[0];
-                this.setState((prevState)=>{
-                    return {
-                        progress: {
-                            value: 0,
-                            infoText: 'Selected Image: ' + path.basename(imagePath)
-                        },
-                        buttonState:{
-                            ums: false,
-                            img: true,
-                            flash: true
-                        }
-                    }
-                });
-            }
-        });
-    }
-
-    writeImage(){
-        drivelist.list((error, drives)=>{
-            drives.forEach((drive)=>{
-                if(drive.description === 'UMS disk 0'){
-
-                    IPCserverStarted.then((socket)=>{
-                        ipc.server.emit(
-                            socket,
-                            'script',
-                            {
-                                id: ipc.config.id,
-                                script: 'imageWrite',
-                                device: drive.device,
-                                size: drive.size,
-                                img: imagePath
-                            }
-                        );
-                    });
-                }
-            });
-        });
-        
     }
 
     render(){
@@ -208,16 +16,16 @@ class MainPage extends React.Component{
             <div>
                 <div id='bl'>    
                     <section id='blocks'>
-                        <Block disabled={!this.state.buttonState.ums} task='USB Mass Storage' imgURL='./assets/usb-memory.png' handleClick={this.umsClick}/>
-                        <Block disabled={!this.state.buttonState.img} task='Select Image' imgURL='./assets/image.png' handleClick={this.selectImage}/>
-                        <Block disabled={!this.state.buttonState.flash} task='Flash' imgURL='./assets/flash.png' handleClick={this.writeImage}/>
+                        <Block disabled={!this.props.umsState} task='USB Mass Storage' imgURL='./assets/usb-memory.png' handleClick={this.props.umsHandle}/>
+                        <Block disabled={!this.props.imgState} task='Select Image' imgURL='./assets/image.png' handleClick={this.props.imgHandle}/>
+                        <Block disabled={!this.props.flashState} task='Flash' imgURL='./assets/flash.png' handleClick={this.props.flashHandle}/>
                     </section>
                 </div>
                 <div id='prog'>
-                    <Progress value={this.state.progress.value}/>
+                    <Progress value={this.props.progressValue}/>
                 </div>
                 <div id='info'>
-                    <Info value={this.state.progress.infoText}/>
+                    <Info value={this.props.infoText}/>
                 </div>
             </div>
         );
