a9ebf859b4da0b94181c345b03896cfe315c8f59
Ravi Kumar Prasad <7ravikp@gmail.com>
2017-08-24 01:39:28 +0530

    fix: osx create window again on app activate

--- main.js
+++ main.js
@@ -52,3 +52,11 @@ function createWindow () {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
+	
+app.on('activate', function () {		
+// On OS X it's common to re-create a window in the app when the		
+// dock icon is clicked and there are no other windows open.		
+ if (mainWindow === null) {
+   createWindow()		
+  }		
+})
