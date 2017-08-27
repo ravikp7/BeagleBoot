a3255d9a7cf110f45baab2d5263e715092552cd5
ravikp7 <7ravikp@gmail.com>
2017-08-27 13:16:21 +0530

    Fixes #3

--- main.js
+++ main.js
@@ -52,6 +52,15 @@ function createWindow () {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
+
+// Quit when all windows are closed.		
+app.on('window-all-closed', function () {		
+ // On OS X it is common for applications and their menu bar		
+// to stay active until the user quits explicitly with Cmd + Q		
+  if (process.platform !== 'darwin') {		
+    app.quit()		
+  }		
+})
 	
 app.on('activate', function () {		
 // On OS X it's common to re-create a window in the app when the		
